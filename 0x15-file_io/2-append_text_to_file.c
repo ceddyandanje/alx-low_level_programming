003944f81d13e7dd36e1799e60fbe3d1f279636b
The root_access_denied <68545507+ceddyandanje@users.noreply.github.com>
2023-10-21 00:34:12 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * append_text_to_file - Appends text at the end of a file.
  * @filename: A pointer to the name of the file.
@@ -21,14 +20,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
-
 	close(o);
-
 	return (1);
 }
